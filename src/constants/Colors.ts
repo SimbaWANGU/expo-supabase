da354aca44ffceb22a85c6ded367ad1e8bbed745
Simba Wangu <simbawangulu@gmail.com>
2023-12-21 20:06:32 +0300

    update constants; colors and interfaces

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -10,12 +10,12 @@ const light = {
 } 
 
 const dark = {
-	appBaseColor: '#001d3d',
-	appBaseColorTwo: '#003566',
+	appBaseColor: '#22444EE',
+	appBaseColorTwo: '#3399FF',
 	appBaseColorThree: '#ffc300',
 	appBaseColorFour: '#ffd60a',
 	appBaseColorFive: '#',
-	text: '#007bff',
+	text: '#f0f0f0',
 	background: '#040404',
 	tint: 'rgba(255, 255, 255, 0.1)'
 }
--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -1,10 +1,28 @@
+import { Session, User } from '@supabase/supabase-js'
+
 interface Store {
   count: number
   increment: () => void
   decrement: () => void
   reset: () => void
+  session: Session | null
+  setSession: (session: Session | null) => void
+  user: User | null
+  setUser: (user: User | null) => void
 }
+
+interface UserProfile {
+  avatar_url: string | null;
+  bio: string | null;
+  emoji: string | null;
+  expopushtoken: string | null;
+  id: string;
+  updated_at: string | null;
+  username: string | null;
+}
+
 
 export {
 	Store,
+	UserProfile
 }
